48c2f490f3d100899d37ca640200a2de12c7befa
Mark Kharitonov <mark.kharitonov@ceridian.com>
2019-12-27 23:26:52 -0500

    Moved modules to a dedicated git repository

---
+++ modules/services/webserver-cluster/variables.tf
@@ -0,0 +1,28 @@
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  type        = number
+  default     = 8080
+}
+
+variable "cluster_name" {
+  description = "The name to use for all the cluster resources"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "The type of EC2 Instances to run (e.g. t2.micro)"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "min_size" {
+  description = "The minimum number of EC2 Instances in the ASG"
+  type        = number
+  default     = 2
+}
+
+variable "max_size" {
+  description = "The maximum number of EC2 Instances in the ASG"
+  type        = number
+  default     = 10
+}
